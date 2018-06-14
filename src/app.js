06392d63ff4affb9e32c1e4264faea04eebf5d85
wiltrahan <twilorip@gmail.com>
2018-06-14 19:26:03 -0400

    nesting components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Action />
+        <Options />
+        <AddOption />
+      </div>
+    )
+  }
+}
+
 class Header extends React.Component {
   render() {
     return (
@@ -24,8 +37,19 @@ class Options extends React.Component {
     return (
       <div>
         <p>Options Component Here</p>
+        <Option />
       </div>
     ); 
+  }
+}
+
+class Option extends React.Component {
+  render() {
+    return (
+      <div>
+        Option Goes Here
+      </div>
+    );
   }
 }
 
@@ -39,13 +63,5 @@ class AddOption extends React.Component {
   }
 }
 
-const jsx = (
-  <div>
-    <Header />
-    <Action />
-    <Options />
-    <AddOption />
-  </div>
-)
 
-ReactDOM.render(jsx, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
