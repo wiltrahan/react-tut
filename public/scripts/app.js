8f7404a402dc5b7510d5c9bb8d98573b254b8c4d
wiltrahan <twilorip@gmail.com>
2018-06-14 14:30:40 -0400

    es6 classes pt1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,44 +1,40 @@
 'use strict';
 
-var app = {
-  title: 'Visiblity Toggle',
-  message: "Life is so crazy, how bout a change of view!"
-};
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-var visiblity = false;
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var changeView = function changeView() {
-  visiblity = !visiblity;
-  changeVisibility();
-};
+var Person = function () {
+  function Person() {
+    var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
+    var age = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
 
-var appRoot = document.getElementById('app');
+    _classCallCheck(this, Person);
 
-var changeVisibility = function changeVisibility() {
-  var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-      'h1',
-      null,
-      app.title
-    ),
-    React.createElement(
-      'button',
-      { onClick: changeView },
-      visiblity ? 'Hide details' : 'Show details'
-    ),
-    visiblity && React.createElement(
-      'div',
-      null,
-      React.createElement(
-        'p',
-        null,
-        app.message
-      )
-    )
-  );
-  ReactDOM.render(template, appRoot);
-};
+    this.name = name;
+    this.age = age;
+  }
 
-changeVisibility();
+  _createClass(Person, [{
+    key: 'getGreeting',
+    value: function getGreeting() {
+      //return 'Hi, I am ' + this.name + '!';
+      return 'Hi, I am ' + this.name + '!';
+    }
+  }, {
+    key: 'getDescription',
+    value: function getDescription() {
+      return this.name + ' is ' + this.age + ' year(s) old.';
+    }
+  }]);
+
+  return Person;
+}();
+
+var me = new Person('Wil Trahan', 38);
+//console.log(me.getGreeting());
+console.log(me.getDescription());
+
+var other = new Person();
+//console.log(other.getGreeting());
+console.log(other.getDescription());
