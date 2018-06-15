ceeab54241edee43478350c11feff983ff5b2cdf
wiltrahan <twilorip@gmail.com>
2018-06-14 20:04:30 -0400

    events & methods

--- src/app.js
+++ src/app.js
@@ -26,22 +26,29 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    alert("HANDLE PICK");
+  }
   render() {
     return (
       <div>
-        <button>What should I do?</button>
+        <button onClick={this.handlePick}>What should I do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  handleRemoveAll() {
+    alert("REMOVE ALL");
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button> 
       {
         this.props.options.map((option) => <Option key ={option} optionText={option} />)
-      } 
+      }
       </div>
     ); 
   }
@@ -58,10 +65,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+    
+    const option = e.target.elements.option.value.trim();
+    
+    if(option) {
+      alert("OPTION TO BE ADDED");
+    }
+  }
   render() {
     return (
       <div>
-        <p>AddOption Component Here</p>
+        <form onSubmit={this.handleAddOption}>
+          <input type="text" name="option"/>
+          <button>Add Option</button>
+        </form>
       </div>
     );   
   }
