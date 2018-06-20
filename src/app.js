8ffce5ea6fee2e18b8c321d4496b8df4af05357f
wiltrahan <twilorip@gmail.com>
2018-06-20 19:38:39 -0400

    babel w/webpack

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
-//import './utils.js';
-// import subtract, { square, add } from './utils.js';
-import isSenior, { isAdult, canDrink } from './person.js';
+// //import './utils.js';
+// // import subtract, { square, add } from './utils.js';
+// import isSenior, { isAdult, canDrink } from './person.js';
 
-console.log('app.js running');
+// console.log('app.js running');
 
-// console.log(square(4));
-// console.log(add(5, 10));
-// console.log(subtract(20, 2));
+// // console.log(square(4));
+// // console.log(add(5, 10));
+// // console.log(subtract(20, 2));
 
-console.log(isAdult(20));
-console.log(canDrink(20));
-console.log(isSenior(55));
+// console.log(isAdult(20));
+// console.log(canDrink(20));
+// console.log(isSenior(55));
+
+//install -> import -> use
+// import validator from 'validator';
+// console.log(validator.isEmail('test@gmail.com'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>Fuck Donald Trump</p>;
+ReactDOM.render(template, document.getElementById('app'));
