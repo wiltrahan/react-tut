0b52a093126cfc853fe49ed689fee4aa602a6766
wiltrahan <twilorip@gmail.com>
2018-06-20 20:06:23 -0400

    component files

--- src/app.js
+++ src/app.js
@@ -1,22 +1,5 @@
-// //import './utils.js';
-// // import subtract, { square, add } from './utils.js';
-// import isSenior, { isAdult, canDrink } from './person.js';
-
-// console.log('app.js running');
-
-// // console.log(square(4));
-// // console.log(add(5, 10));
-// // console.log(subtract(20, 2));
-
-// console.log(isAdult(20));
-// console.log(canDrink(20));
-// console.log(isSenior(55));
-
-//install -> import -> use
-// import validator from 'validator';
-// console.log(validator.isEmail('test@gmail.com'));
 import React from 'react';
 import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
 
-const template = <p>Fuck Donald Trump</p>;
-ReactDOM.render(template, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
