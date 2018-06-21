a0281aa6a3050a3dfe59e3599a68ebc0851af96c
wiltrahan <twilorip@gmail.com>
2018-06-21 16:08:48 -0400

    button

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -72,22 +72,24 @@ class IndecisionApp extends React.Component {
     return (
       <div>
         <Header />
-        <Action 
-          hasOptions={this.state.options.length > 0} 
-          handlePick={this.handlePick}
-        />
-        <Options 
-          options={this.state.options} 
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption 
-          handleAddOption={this.handleAddOption}
-        />
-        <OptionModal
-          selectedOption={this.state.selectedOption}
-          handleClearSelectedOption={this.handleClearSelectedOption}
-        />
+        <div className="container">
+          <Action 
+            hasOptions={this.state.options.length > 0} 
+            handlePick={this.handlePick}
+          />
+          <Options 
+            options={this.state.options} 
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption 
+            handleAddOption={this.handleAddOption}
+          />
+          </div>
+          <OptionModal
+            selectedOption={this.state.selectedOption}
+            handleClearSelectedOption={this.handleClearSelectedOption}
+          />
       </div>
     );
   }
